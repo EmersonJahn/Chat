09a2ebd4c630a1c698c1fd0e7444d069c0eb13d1
Emerson Adierson Jahn <emersonajahn@gmail.com>
2021-01-27 12:59:08 -0300

    Implement login.

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -2,8 +2,10 @@ import 'dart:io';
 
 import 'package:chat/text_composer.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class ChatScreen extends StatefulWidget {
   @override
@@ -12,9 +14,65 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen> {
 
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  FirebaseUser _currentUser;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    FirebaseAuth.instance.onAuthStateChanged.listen((user) {
+      _currentUser = user;
+    });
+  }
+
+  Future<FirebaseUser> _getUser() async {
+
+    if (_currentUser != null) return await _getUser();
+
+    try {
+
+      final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
+      final GoogleSignInAuthentication googleSignInAuthentication = await googleSignInAccount.authentication;
+      
+      final AuthCredential credential = GoogleAuthProvider.getCredential(
+        idToken: googleSignInAuthentication.idToken, 
+        accessToken: googleSignInAuthentication.accessToken,
+      );
+
+      final AuthResult authResult = await FirebaseAuth.instance.signInWithCredential(credential);
+
+      final FirebaseUser user = authResult.user;
+
+      return user;
+
+    } catch (e) {
+      return null;
+
+    }
+  }
+
   void _sendMessage({String text, File imgFile}) async {
 
-    Map<String, dynamic> data = {};
+    final FirebaseUser user = await _getUser();
+
+    if(user == null) {
+      _scaffoldKey.currentState.showSnackBar(
+        SnackBar(
+          content: Text("Não foi possível fazer o login. Tente novamente!"),
+          backgroundColor: Colors.red,
+        )
+      );
+    }
+
+    Map<String, dynamic> data = {
+      "uid": user.uid,
+      "senderName": user.displayName,
+      "senderPhotoUrl": user.photoUrl
+    };
 
     if (imgFile != null) {
       StorageUploadTask task = FirebaseStorage.instance.ref().child(
@@ -29,11 +87,13 @@ class _ChatScreenState extends State<ChatScreen> {
     if (text != null) data['text'] = text;
 
     Firestore.instance.collection('messages').add(data);
+
   } 
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         title: Text("Olá"),
         elevation: 0,
